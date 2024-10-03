8ad38ff44d3f04f4e0a716c4859fbc560a351cdf
CHIHAYATAKU <chihaya.takuma@nextbeat.net>
2024-10-03 11:44:41 +0900

    add&update: todo追加機能の追加

--- app/controllers/ToDo.scala
+++ app/controllers/ToDo.scala
@@ -1,10 +1,9 @@
 package controllers
 
-import play.api.data._
-import play.api.data.Forms._
 import javax.inject._
 import play.api.mvc._
 import scala.concurrent._
+import models.forms.ToDoDataForm
 import persistence.{ ToDoRepository, ToDoCategoryRepository }
 import model.{ ToDo, ToDoCategory }
 import model.viewValue.ViewValueToDo
@@ -32,6 +31,36 @@ class ToDoController @Inject() (
       ))
     }
   }
+
+  def showCreateToDoForm() = Action.async { implicit request: Request[AnyContent] =>
+    categoryRepo.getAll().map { categories =>
+      Ok(views.html.ToDoCreateForm(ToDoDataForm.form, categories)(request, messagesApi.preferred(request)))
+    }
+  }
+
+def createToDo() = Action.async { implicit request: Request[AnyContent] =>
+  // カテゴリを取得
+  categoryRepo.getAll().flatMap { categories =>
+    ToDoDataForm.form.bindFromRequest().fold(
+      formWithErrors => {
+        // BadRequest時にカテゴリをビューに渡す
+        Future.successful(BadRequest(views.html.ToDoCreateForm(formWithErrors, categories)(request, messagesApi.preferred(request))))
+      },
+      toDoData => {
+        val newTodo = ToDo(
+          id         = None,
+          categoryId = ToDoCategory.Id(toDoData.categoryId),
+          title      = toDoData.title,
+          body       = toDoData.body,
+          state      = ToDo.ToDoState.TODO
+        ).toWithNoId
+        todoRepo.add(newTodo).map { _ =>
+          Redirect(routes.ToDoController.index()).flashing("success" -> "ToDoが追加されました！")
+        }
+      }
+    )
+  }
+}
 
   // ToDoの削除メソッド
   def deleteToDo(id: Long) = Action.async { implicit request: Request[AnyContent] =>
