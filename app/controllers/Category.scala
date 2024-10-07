cf75076d86b74a98c333bf4cd64dedd8218d8c6a
CHIHAYATAKU <chihaya.takuma@nextbeat.net>
2024-10-07 13:26:20 +0900

    add: カテゴリページの追加

---
+++ app/controllers/Category.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import scala.concurrent._
+import persistence.ToDoCategoryRepository
+import model.ToDoCategory
+import model.viewValue.ViewValueCategory
+
+@Singleton
+class CategoryController @Inject() (
+  val controllerComponents: ControllerComponents,
+  categoryRepo:             ToDoCategoryRepository
+)(implicit ec:              ExecutionContext) extends BaseController {
+
+  def index() = Action.async { implicit request: Request[AnyContent] =>
+    categoryRepo.getAll().map { categories =>
+      Ok(views.html.Category(
+        ViewValueCategory(
+          title      = "Categories",
+          cssSrc     = Seq("Category.css"),
+          jsSrc      = Seq("Category.js"),
+          categories = categories
+        )
+      ))
+    }
+  }
+}
